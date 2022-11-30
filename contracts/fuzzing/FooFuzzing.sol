ac6fb681ddfad81f0159d8786d07648e8573ecd3
95decode <95decode@gmail.com>
2022-11-30 12:46:08 +0900

    Fix typo

--- contracts/fuzzing/FooFuzzing.sol
+++ contracts/fuzzing/FooFuzzing.sol
@@ -8,25 +8,6 @@ contract FooFuzzing is Foo {
     constructor() {}
 
     function echidna_deposit() public view returns (bool) {
-        return balance[msg.sender] < MAX;
+        return balance[msg.sender] <= MAX;
     }
 }
-
-/*
-contract FooFuzzing {
-    Foo internal foo;
-    //address internal bar;
-
-    constructor() {
-        foo = new Foo();
-    }
-
-    function deposit(uint256 _amount) external {
-        foo.deposit(_amount);
-    }
-
-    function echidna_deposit() public view returns (bool) {
-        return foo.balance(msg.sender) < 100;
-    }
-}
-*/
