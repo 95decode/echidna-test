a89155c7392d43467b7098f2244706ab57cee4e9
95decode <95decode@gmail.com>
2022-12-07 11:50:51 +0900

    Update solidity compiler conifg

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,38 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [{
+      version: "0.8.9",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+          details: {
+            yul: true,
+            yulDetails : {
+              stackAllocation : true
+            },
+          },
+        },
+      },
+    },{
+      version: "0.8.16",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+          details: {
+            yul: true,
+            yulDetails : {
+              stackAllocation : true
+            },
+          },
+        },
+      },
+    }],
+  },
 };
+
 
 export default config;
